b763e1c1be4e3341ea9c344812eb72c586883bbe
betadoggy <mgs4786@gmail.com>
2020-09-10 17:41:27 +0900

    remove unnecessary files

--- file_search.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path = "crawled"
-file_list = os.listdir(path)
-file_list_txt = [file for file in file_list if file.endswith(".txt")]
-
-print ("file_list_txt: {}".format(file_list))
-
--- folder_make.py
+++
@@ -1,6 +0,0 @@
-import os
-
-today = str(date.today())
-
-if not(os.path.isdir(today)):
-   os.mkdir(os.path.join(today))
